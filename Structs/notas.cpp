b42811451b6e41e06610d4d60f817a4ebacc4f60
Emily Horrana <128863617+emyHorrana@users.noreply.github.com>
2025-05-04 22:13:55 -0300

    code from INATEL tasks

--- Structs/notas.cpp
+++ Structs/notas.cpp
@@ -2,19 +2,24 @@
 #include <iomanip>
 using namespace std;
 
+struct Aluno{
+    int matricula;
+    float nota1, nota2;
+    float media;
+};
+
 int main(){
     
-    struct{
-        int matricula;   
-        float nota1;
-        float nota2;
-    }aluno;
+    Aluno aluno;
     
     cin>>aluno.matricula>>aluno.nota1>>aluno.nota2;
+    
+    aluno.media = ((aluno.nota1*2) + (aluno.nota2 *3))/5.0;
     cout<<fixed<<setprecision(2);
     cout<<"Matricula do aluno: "<<aluno.matricula<<endl;
     cout<<"Nota da primeira prova: "<<aluno.nota1<<endl;
     cout<<"Nota da segunda prova: "<<aluno.nota2<<endl;
+    cout<<"Media: "<<aluno.media;
     
     return 0;
 }
